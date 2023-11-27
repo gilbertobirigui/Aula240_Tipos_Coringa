d8dc60c15fb8584cb56a78f26e15fb7d4d3cf149
gilbertobirigui <gfilmesbirigui@hotmail.com>
2023-11-27 10:21:20 -0300

    Aula 240 Tipos Coringa

---
+++ src/aula240tiposcuringa/Aula240TiposCuringa.java
@@ -0,0 +1,39 @@
+
+
+/*
+
+Com tipos curinga podemos fazer metodos que recebem um generico de qualquer tipo
+
+
+*/
+
+package aula240tiposcuringa;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+
+// tipo coringa = ? interrogacao 
+
+public class Aula240TiposCuringa {
+
+   
+  
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+        
+        List<String> myStrs = Arrays.asList("maria", "joao", "bob");
+        printList(myStrs);
+    }
+    
+    
+    // esse method funciona pra qualquer tipo usando coringa = ?
+    public static void printList(List<?> list){
+        for (Object obj : list){
+            System.out.println(obj);
+        }
+    }
+    
+}
